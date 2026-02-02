7a6300c614c321750bd06ec055c9bb17458986d1
Kosuke Uchida <byebyeearthjpn@gmail.com>
2026-02-02 13:11:26 +0900

    fixed

--- src/header.js
+++ src/header.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         Magazine Comic Viewer Helper
-// @name:ja      マガジン・コミック・ビューア・ヘルパー
-// @author       kuchida1981
-// @namespace    https://github.com/kuchida1981/comic-viewer-helper
-// @version      1.1.0
-// @description  A Tampermonkey script for specific comic sites that fits images to the viewport and enables precise image-by-image scrolling.
-// @description:ja 特定の漫画サイトで画像をビューポートに合わせ、画像単位のスクロールを可能にするユーザースクリプトです。
-// @license      ISC
-// @match        https://something/magazine/*
-// @match        https://something/fanzine/*
-// @updateURL    https://raw.githubusercontent.com/kuchida1981/comic-viewer-helper/stable/comic-viewer-helper.user.js
-// @downloadURL  https://raw.githubusercontent.com/kuchida1981/comic-viewer-helper/stable/comic-viewer-helper.user.js
-// @run-at       document-idle
-// @grant        none
+// @name            Magazine Comic Viewer Helper
+// @name:ja         マガジン・コミック・ビューア・ヘルパー
+// @author          kuchida1981
+// @namespace       https://github.com/kuchida1981/comic-viewer-helper
+// @version         1.1.0
+// @description     A Tampermonkey script for specific comic sites that fits images to the viewport and enables precise image-by-image scrolling.
+// @description:ja  特定の漫画サイトで画像をビューポートに合わせ、画像単位のスクロールを可能にするユーザースクリプトです。
+// @license         ISC
+// @match           https://something/magazine/*
+// @match           https://something/fanzine/*
+// @updateURL       https://raw.githubusercontent.com/kuchida1981/comic-viewer-helper/stable/comic-viewer-helper.user.js
+// @downloadURL     https://raw.githubusercontent.com/kuchida1981/comic-viewer-helper/stable/comic-viewer-helper.user.js
+// @run-at          document-idle
+// @grant           none
 // ==/UserScript==
