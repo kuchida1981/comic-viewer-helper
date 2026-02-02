493268e4fa4a435f4ced7f3cc662e2486705330f
Kosuke Uchida <byebyeearthjpn@gmail.com>
2026-02-02 13:07:32 +0900

    docs: add support for Greasy Fork and update metadata

--- src/header.js
+++ src/header.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name         Magazine Comic Viewer Helper
+// @name:ja      マガジン・コミック・ビューア・ヘルパー
 // @author       kuchida1981
 // @namespace    https://github.com/kuchida1981/comic-viewer-helper
 // @version      1.1.0
 // @description  A Tampermonkey script for specific comic sites that fits images to the viewport and enables precise image-by-image scrolling.
+// @description:ja 特定の漫画サイトで画像をビューポートに合わせ、画像単位のスクロールを可能にするユーザースクリプトです。
+// @license      ISC
 // @match        https://something/magazine/*
 // @match        https://something/fanzine/*
 // @updateURL    https://raw.githubusercontent.com/kuchida1981/comic-viewer-helper/stable/comic-viewer-helper.user.js
