01d8c229b9cd6efa78889f20c703d5b3b510df90
kuchida1981 <byebyeearthjpn@gmail.com>
2026-02-06 07:45:39 +0000

    Deploying to unstable from @ kuchida1981/comic-viewer-helper@15d8afd032414f67c91019b1c17d1027a840bbd4 🚀

--- comic-viewer-helper.user.js
+++ comic-viewer-helper.user.js
@@ -3,7 +3,7 @@
 // @name:ja         マガジン・コミック・ビューア・ヘルパー
 // @author          kuchida1981
 // @namespace       https://github.com/kuchida1981/comic-viewer-helper
-// @version         1.3.0-unstable.c4d9ee4
+// @version         1.3.0-unstable.15d8afd
 // @description     A Tampermonkey script for specific comic sites that fits images to the viewport and enables precise image-by-image scrolling.
 // @description:ja  特定の漫画サイトで画像をビューポートに合わせ、画像単位のスクロールを可能にするユーザースクリプトです。
 // @license         ISC
@@ -1710,7 +1710,7 @@
         borderTop: "1px solid #eee",
         paddingTop: "5px"
       },
-      textContent: `${t("ui.version")}: v${"1.3.0-unstable.c4d9ee4"} (${t("ui.unstable")})`
+      textContent: `${t("ui.version")}: v${"1.3.0-unstable.15d8afd"} (${t("ui.unstable")})`
     });
     const content = createElement("div", {
       className: "comic-helper-modal-content",
